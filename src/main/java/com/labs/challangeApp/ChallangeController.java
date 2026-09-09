8e6823ee5de16fd3aea07098ba63f51e7029fac3
Hadi Prasetyo <33802468+hadipsy27@users.noreply.github.com>
2026-09-09 16:42:36 +0700

    create service add a new challenge

--- src/main/java/com/labs/challangeApp/ChallangeController.java
+++ src/main/java/com/labs/challangeApp/ChallangeController.java
@@ -1,6 +1,8 @@
 package com.labs.challangeApp;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -19,5 +21,11 @@ public class ChallangeController {
     @GetMapping("/challanges")
     public List<Challange> getAllChallanges() {
         return challanges;
+    }
+
+    @PostMapping("/challanges")
+    public String addChallange(@RequestBody Challange challange){
+        challanges.add(challange);
+        return "Challange added successfully";
     }
 }
