580f2b73bb73616e065f0b5f89eb7a28a685606c
Hadi Prasetyo <33802468+hadipsy27@users.noreply.github.com>
2026-09-11 16:51:18 +0700

    CRUD challenge with JPA

--- src/main/java/com/labs/challangeApp/ChallengeRepository.java
+++ src/main/java/com/labs/challangeApp/ChallengeRepository.java
@@ -2,6 +2,9 @@ package com.labs.challangeApp;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ChallengeRepository extends JpaRepository<Challenge, Long> {
 
+    Optional<Challenge> findByMonthIgnoreCase(String month);
 }
